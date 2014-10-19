ad3c579900ea915c8a78c13e99a0a929a15da15b
qxsch <qxsch@users.noreply.github.com>
2014-10-19 23:52:01 +0200

    little typo fix in the examples

--- examples/simpleClient.php
+++ examples/simpleClient.php
@@ -6,7 +6,7 @@ require_once(dirname(__DIR__).'/SimpleSocket.php');
 require_once(dirname(__DIR__).'/TCPClient.php');
 
 
-echo "Sending: Hi\n";
+echo "Sending: hi\n";
 
 $client = new \QXS\MultiProcessServer\TCPClient(12345);  // connect to 127.0.0.1 on port 12345
 $client->send("hi");
