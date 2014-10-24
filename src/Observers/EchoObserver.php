a70fdf272d9af4c3a94cfb98346697788c589399
qxsch <qxsch@users.noreply.github.com>
2014-10-25 00:12:49 +0200

    better logging

--- src/Observers/EchoObserver.php
+++ src/Observers/EchoObserver.php
@@ -21,15 +21,18 @@ class EchoObserver implements ObserverInterface {
 	public function update(\SplSubject $subject, $eventType=ObserverInterface::EV_UNKNOWN, array $metaData=array()) {
 		echo '['.date('Y-m-d H:i:s').']['.get_class($subject).'] Received an event of type ';
 		switch($eventType) {
-			case ObserverInterface::EV_SERVER_IMPERSONATE: echo "SERVER IMPERSONATE"; break;
-			case ObserverInterface::EV_SERVER_START: echo "SERVER START"; break;
-			case ObserverInterface::EV_SERVER_STOP: echo "SERVER STOP"; break;
+			// event loop
 			case ObserverInterface::EV_SERVER_WAITING_FOR_FREE_FORKS: echo "SERVER WAITING FOR FREE FORKS"; break;
 			case ObserverInterface::EV_SERVER_WAITING_FOR_INCOMING_CONNECTION: echo "SERVER WAITING FOR INCOMING CONNECTIONS"; break;
 			case ObserverInterface::EV_SERVER_NEW_INCOMING_CONNECTION: echo "SERVDER NEW INCOMING CONNECTION"; break;
 			case ObserverInterface::EV_CLIENT_FORKED: echo "CLIENT FORKED"; break;
 			case ObserverInterface::EV_CLIENT_TERMINATED: echo "CLIENT TERMINATED"; break;
-			default: echo 'UNKNOWN';
+			// startup
+			case ObserverInterface::EV_SERVER_IMPERSONATE: echo "SERVER IMPERSONATE"; break;
+			case ObserverInterface::EV_SERVER_START: echo "SERVER START"; break;
+			case ObserverInterface::EV_SERVER_STOP: echo "SERVER STOP"; break;
+			// unknown exception
+			default: echo 'UNKNOWN VALUE '.((int)$eventType);
 		}
 		echo "\n";
 		if(!empty($metaData)) {
